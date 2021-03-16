8345f561e35b74e1ebc1aae82d1110517b25c9e5
Stiliyana Petkova <76534910+Stiliyana-Petkova@users.noreply.github.com>
2021-03-16 15:18:39 +0200

    Add files via upload

---
+++ Arrays/sortingBy2.js
@@ -0,0 +1,22 @@
+function solve(array) {
+    array.sort((a,b) => a.localeCompare(b));
+    array.sort((a,b) => a.length - b.length);
+
+    
+      return array.join('\n');  
+
+    
+}
+
+console.log(solve(['alpha', 
+'beta', 
+'gamma']));
+console.log(solve(['Isacc', 
+'Theodor', 
+'Jack', 
+'Harrison', 
+'George']));
+console.log(solve(['test', 
+'Deny', 
+'omen', 
+'Default']));
