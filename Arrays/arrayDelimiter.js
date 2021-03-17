066e94b39905c877a73e03da6120cfb417a3a4f8
Stiliyana Petkova <76534910+Stiliyana-Petkova@users.noreply.github.com>
2021-03-17 13:49:24 +0200

    Add files via upload

---
+++ Arrays/arrayDelimiter.js
@@ -0,0 +1,17 @@
+function solve(array, str) {
+    console.log(array.join(str));
+}
+
+solve(['One', 
+'Two', 
+'Three', 
+'Four', 
+'Five'], 
+'-');
+solve(['How about no?', 
+'I',
+'will', 
+'not', 
+'do', 
+'it!'], 
+'_');
