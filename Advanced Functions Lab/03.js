019a0b134d33cc9cfa2ad60196656f0be6f62d6d
Stiliyana Petkova <76534910+Stiliyana-Petkova@users.noreply.github.com>
2021-03-19 10:10:32 +0200

    Add files via upload

---
+++ Advanced Functions Lab/03.js
@@ -0,0 +1,23 @@
+function createFormatter(a, b, boolean, func) {
+
+   // return (value) => func(a, b, boolean, value);
+
+   return function add(n) {
+       return func(a, b, boolean, n)
+   }
+   
+   
+
+}
+
+let dollarFormatter = createFormatter(',', '$', true, currencyFormatter);
+console.log(dollarFormatter(5345));   // $ 5345,00
+console.log(dollarFormatter(3.1429)); // $ 3,14
+console.log(dollarFormatter(2.709));  // $ 2,71
+
+function currencyFormatter(separator, symbol, symbolFirst, value) {
+    let result = Math.trunc(value) + separator;
+    result += value.toFixed(2).substr(-2,2);
+    if (symbolFirst) return symbol + ' ' + result;
+    else return result + ' ' + symbol;
+}
